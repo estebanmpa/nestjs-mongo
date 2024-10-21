06c6a8a62bd06f69b851e9cbc6dc17d736c26fab
Esteban <esteban.piga.developer@gmail.com>
2024-10-21 20:16:14 -0300

    Add deploy with github actions Modify mongodb connection string

--- src/common/configuration/configuration.ts
+++ src/common/configuration/configuration.ts
@@ -4,6 +4,7 @@
 export default() => ({
     listenPort: parseInt(process.env.LISTEN_PORT) || 3000,
     database: {
-        connectionString: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
+        connectionString: process.env.MONGO_CONNECTION_STRING
+        //connectionString: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
     }
 })
